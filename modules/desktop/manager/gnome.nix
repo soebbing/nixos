11e13289951a911fe98d77903ef0ede65358605f
Hendrik Söbbing <hendrik@soebbing.de>
2025-07-23 10:52:07 +0200

    Add gnome themes

--- modules/desktop/manager/gnome.nix
+++ modules/desktop/manager/gnome.nix
@@ -3,22 +3,6 @@
 with pkgs;
 let
   unstable = import <nixos-unstable> {};
-#  gnome-shell-extension-pop-shell = stdenv.mkDerivation rec {
-#    pname = "gnome-shell-extension-pop-shell";
-#    version = "1.2.0";
-#    src = fetchFromGitHub {
-#      owner = "pop-os";
-#      repo = "shell";
-#      rev = "891525b74b706683301d7415a62b012472a03ad4";
-#      sha256 = "1ba1nrnk4cqgjx5mygqdkw74xlankrkiib9rw0vwkjcgv9bj024a";
-#    };
-
-#    nativeBuildInputs = [ glib ];
-#    buildInputs = [ nodePackages.typescript ];
-
-    # the gschema doesn't seem to be installed properly (see dconf)
-#    makeFlags = [ "INSTALLBASE=$(out)/share/gnome-shell/extensions" ];
-#  };
 
 in {
   nixpkgs.config.allowUnfree = true;
@@ -32,19 +16,25 @@ in {
     unstable.gnomeExtensions.sound-output-device-chooser
     unstable.rhythmbox
     unstable.baobab
+
+    unstable.adwaita-icon-theme
     unstable.numix-icon-theme
     unstable.numix-solarized-gtk-theme
-#    gnome-shell-extension-pop-shell
+    unstable.numix-cursor-theme
+    unstable.breeze-hacked-cursor-theme
+    unstable.openzone-cursors
+
+    gnomeExtensions.pop-shell
   ];
 
   services.xserver = {
     enable = true;
     displayManager.gdm = {
       enable = true;
-      wayland = false;
+      wayland = true;
       autoSuspend = true;
     };
-    desktopManager = { gnome = { enable = true; }; };
+    desktopManager.gnome.enable = true;
   };
 
   services.gvfs.enable = true;
