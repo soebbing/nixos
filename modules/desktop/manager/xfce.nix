9b88fb2fc97181227df605420013a6eae8c546df
Hendrik Söbbing <hendrik@soebbing.de>
2026-03-16 20:37:32 +0100

    Update XFCE settings

--- modules/desktop/manager/xfce.nix
+++ modules/desktop/manager/xfce.nix
@@ -34,37 +34,94 @@
   };
 
   environment.systemPackages = with pkgs; [
+    # Application launchers & menus
     rofi
+    rofi-pass
+
+    # Display & monitor management
     arandr
+    autorandr
+    xrandr
+
+    # Media control & audio
     playerctl
+    pamixer
+    pavucontrol
+
+    # Bluetooth
     blueman
+
+    # Screenshots & screen recording
     feh
     escrotum
+    xfce4-screenshooter
+    xfce4-clipman-plugin
 
+    # Themes & appearance
     numix-icon-theme
     numix-solarized-gtk-theme
+    gnome-themes-extra
+    papirus-icon-theme
+    arc-theme
+    adwaita-qt
+    libsForQt5.qtstyleplugin-kvantum
 
-    xfce.exo
-    xfce.parole
-    xfce.ristretto
-    xfce.tumbler
-    xfce.thunar
-    xfce.xfconf
-    xfce.xfce4-icon-theme
-    xfce.xfce4-session
-    xfce.xfce4-settings
-    xfce.xfce4-taskmanager
-    xfce.xfce4-screenshooter
-    xfce.xfce4-pulseaudio-plugin
-    xfce.xfce4-cpugraph-plugin
-    xfce.xfce4-power-manager
-    xfce.xfce4-netload-plugin
-    xfce.xfce4-clipman-plugin
-    xfce.xfce4-sensors-plugin
-    xfce.xfce4-volumed-pulse
-    xfce.xfce4-weather-plugin
-    xfce.xfce4-xkb-plugin
-    xfce.xfwm4
-    xfce.xfwm4-themes
+    # XFCE core components
+    exo
+    parole
+    ristretto
+    tumbler
+    thunar
+    thunar-archive-plugin
+    thunar-volman
+    xfconf
+    xfce4-icon-theme
+    xfce4-session
+    xfce4-settings
+    xfce4-taskmanager
+    xfce4-terminal
+    xfce4-whiskermenu-plugin
+    xfce4-docklike-plugin
+    xfce4-pulseaudio-plugin
+    xfce4-cpugraph-plugin
+    xfce4-power-manager
+    xfce4-netload-plugin
+    xfce4-sensors-plugin
+    xfce4-volumed-pulse
+    xfce4-weather-plugin
+    xfce4-xkb-plugin
+    xfwm4
+    xfwm4-themes
+
+    # System utilities
+    gnome-disk-utility
+    gparted
+    baobab
+    #galculator
+    catfish
+    engrampa
+    xfce4-appfinder
+
+    # Network & connectivity
+    networkmanagerapplet
+    networkmanager-openvpn
+
+    # Power management
+    acpi
+    tlp
+    powertop
+
+    # Input devices
+    xf86-input-wacom
+    libwacom
+
+    # Notification daemon
+    dunst
+    libnotify
+
+    # Desktop enhancements
+    picom
+    nitrogen
+    variety
   ];
 }
